42683cbff34c8d4dc0f091008766133b96e6821a
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-12-21 13:53:38 +0100

    import and use vue notification in the main js file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faAngleRight } from '@fortawesome/free-solid-svg-icons'
@@ -9,4 +11,4 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faUser, faAngleRight)
 
 
-createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router).use(Toast).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
